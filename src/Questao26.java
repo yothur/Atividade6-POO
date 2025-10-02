2b75b43597b5c757c3b111c0e40caa0de7f2e56b
Arthur <arthurgren4@gmail.com>
2025-10-02 13:56:28 -0300

    miniuptade

--- src/Questao26.java
+++ src/Questao26.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 public class Questao26 {
 
 
     public static int[] semRepetir(){
         int indice = 0;
-        int[] principal = new int[]{2,2,3,4,5,6,6,8,9,9};
+        int[] principal = new int[]{2,7,3,4,5,6,6,2,9,9};
+        Arrays.sort(principal);
         int[] novo = new int[7];
         novo[indice] = principal[0];
         int maior = principal[0];
